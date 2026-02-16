bae24824cc699951bbba29fcab5cdefaebd5491a
Greg Atemi <magrega7180@gmail.com>
2026-02-16 08:52:12 +0300

    Added Postgres DB connection to application

--- src/main/java/com/magrega/demo/controller/ProductController.java
+++ src/main/java/com/magrega/demo/controller/ProductController.java
@@ -2,27 +2,38 @@ package com.magrega.demo.controller;
 
 import com.magrega.demo.model.Product;
 import com.magrega.demo.service.ProductService;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
+@RequestMapping("/api")
 public class ProductController
 {
     @Autowired
     ProductService service;
 
     @GetMapping("/products")
-    public List<Product> getProducts(){
-        return service.getProducts();
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(service.getProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/product/{prodId}")
-    public Product getProductById(@PathVariable int prodId)
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id)
     {
-        return service.getProductById(prodId);
+        Product product = service.getProductById(id);
+
+        if (product != null)
+        {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/product/{prodId}")
--- src/main/java/com/magrega/demo/model/Product.java
+++ src/main/java/com/magrega/demo/model/Product.java
@@ -1,17 +1,32 @@
 package com.magrega.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @Component
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product
 {
     @Id
-    private int prodId;
-    private String prodName;
-    private int price;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
 }
