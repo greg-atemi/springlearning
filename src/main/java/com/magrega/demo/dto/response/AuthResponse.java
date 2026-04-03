b23b22c1e9ef10ec6f39f53bd8cb7a0ff80700ec
Greg Atemi <magrega7180@gmail.com>
2026-04-03 10:41:54 +0300

    Added Registration and Login endpoints

---
+++ src/main/java/com/magrega/demo/dto/response/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.magrega.demo.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+}
